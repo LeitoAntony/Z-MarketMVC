8770a8cdbf749e3888bb597c3c2a8d3ffba294ff
LeitoAntony <leitoantony86@gmail.com>
2017-06-27 15:40:46 -0300

    first commit

---
+++ Z-Market/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Z_Market.Models
+{
+    public class Product
+    {
+
+        //Data notation, autonumerico
+        [Key]
+        public int ProductId { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime LastBuy { get; set; }
+        public float Stock { get; set; }
+        public string Remarks { get; set; }
+    }
+}
