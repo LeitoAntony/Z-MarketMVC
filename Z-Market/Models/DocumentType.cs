dfd0f13d69aea0bd039d77cfd6c02bbe08e292e8
LeitoAntony <leitoantony86@gmail.com>
2017-06-28 15:56:05 -0300

    modificacion de data anotations

--- Z-Market/Models/DocumentType.cs
+++ Z-Market/Models/DocumentType.cs
@@ -10,8 +10,11 @@ namespace Z_Market.Models
     {
         [Key]
         public int DocumentId { get; set; }
+
+        [Display(Name = "Document")]
         public string Description { get; set; }
 
+        [Display(Name = "Document Type")]
         public virtual ICollection<Employee> Employees { get; set; }
     }
 }
--- Z-Market/Models/Employee.cs
+++ Z-Market/Models/Employee.cs
@@ -10,15 +10,35 @@ namespace Z_Market.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "You must enter {0}")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "You must enter {0}")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "You must enter {0}")]
         public decimal Salary { get; set; }
+
+        [Display(Name = "Bonus Percent")]
         public float BonusPercent { get; set; }
+
+        [Display(Name = "Date Of Birth")]
         public DateTime DateOfBirth { get; set; }
+
+        [Display(Name = "Start Time")]
+        [Required(ErrorMessage = "You must enter {0}")]
         public DateTime StartTime { get; set; }
+        
         public string EMail { get; set; }
+
         public string URL { get; set; }
         //foreign key
+
+        [Display(Name = "Document Type")]
+        [Required(ErrorMessage = "You must enter {0}")]
         public int DocumentId { get; set; }
 
         public virtual DocumentType DocumentType { get; set; }
